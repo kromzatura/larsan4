d4062e65426437a8ccf9116496020504de21645e
kromzatura <kromzatura@gmail.com>
2025-10-13 14:43:24 +0200

    fix ed contact quiry

--- frontend/sanity/queries/contact.ts
+++ frontend/sanity/queries/contact.ts
@@ -2,17 +2,27 @@ import { groq } from "next-sanity";
 import { linkQuery } from "./shared/link";
 import { metaQuery } from "./shared/meta";
 
-export const CONTACT_QUERY = groq`*[_type == "contact"][0]{
-  tagline,
-  title,
-  description,
-  contactMethods[]{
-    icon,
+export const CONTACT_QUERY = groq`
+  *[
+    _type == "contact" &&
+    (!defined(language) || language in [$lang, $fallbackLang])
+  ]
+  | order(
+    (language == $lang) desc,
+    (language == $fallbackLang) desc,
+    _updatedAt desc
+  )[0]{
+    tagline,
     title,
     description,
-    link {
-      ${linkQuery}
-    }
-  },
-  ${metaQuery},
-}`;
+    contactMethods[]{
+      icon,
+      title,
+      description,
+      link {
+        ${linkQuery}
+      }
+    },
+    ${metaQuery},
+  }
+`;
